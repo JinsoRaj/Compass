1c2cee7c564487d7bc3784476c9b785cd5eab792
Jinso Raj <jinsoraj2000@gmail.com>
2025-06-05 00:43:32 +0530

    assessment response submissions

--- compass/api.py
+++ compass/api.py
@@ -1,6 +1,7 @@
 # # compass/api.py
-# import frappe
-# from frappe import _
+import frappe
+import json
+from frappe import _
 
 # @frappe.whitelist()
 # def get_options_for_display(question, txt=None):
@@ -20,3 +21,36 @@
 #     )
 
 #     return [{"value": opt["name"], "label": opt["option_text"]} for opt in options]
+
+
+
+@frappe.whitelist(allow_guest=True)
+def submit_assessment_response(assessment, responses):
+    # Create new response document
+    response = frappe.new_doc("Assessment Response")
+    response.assessment = assessment
+
+   #  frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(responses)))
+   #  frappe.msgprint(responses)
+    
+    # Add answers
+    parsed_data = json.loads(responses)
+   #  frappe.msgprint("<pre>{}</pre>".format(parsed_data))
+   #  frappe.msgprint("<pre>{}</pre>".format(parsed_data))
+    for res in parsed_data:
+      #   frappe.msgprint("<pre>{}</pre>".format(res)) # this works
+      #   print("test")
+      #   frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(res)))
+      #   option_doc = frappe.get_doc("Question Option", res['selected_option'])
+        response.append("answers", {
+            "question": res['question'],
+            # "question": '2nd question - asm1-18-05-02',
+            "selected_option": res['selected_option'],
+            # "selected_option": 'op1-qn2-19-06',
+            # "selected_option_text": option_doc.option_text,
+            # "weight": option_doc.weight
+        })
+    
+    # Insert and return response name
+    response.insert(ignore_permissions=True)
+    return response.name
